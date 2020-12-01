d284cd66c3df5a8235c5e0b989e9a316201029e9
rabeeh <rabeeh@google.com>
2020-12-01 17:13:53 +0100

    code cleanup

--- seq2seq/models/t5/modeling_outputs.py
+++ seq2seq/models/t5/modeling_outputs.py
@@ -12,31 +12,45 @@ from transformers.modeling_outputs import BaseModelOutput
 @dataclass
 class RuseBaseModelOutputWithPastAndCrossAttentions(BaseModelOutput):
     """
-    Base class for model's outputs that may also contain a past key/values (to speed up sequential decoding).
+    Base class for model's outputs that may also contain a past key/values (to
+    speed up sequential decoding).
     Args:
-        last_hidden_state (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`):
+        last_hidden_state (:obj:`torch.FloatTensor` of shape :obj:`(batch_size,
+         sequence_length, hidden_size)`):
             Sequence of hidden-states at the output of the last layer of the model.
-            If :obj:`past_key_values` is used only the last hidden-state of the sequences of shape :obj:`(batch_size,
-            1, hidden_size)` is output.
-        past_key_values (:obj:`List[torch.FloatTensor]`, `optional`, returned when ``use_cache=True`` is passed or when ``config.use_cache=True``):
-            List of :obj:`torch.FloatTensor` of length :obj:`config.n_layers`, with each tensor of shape :obj:`(2,
-            batch_size, num_heads, sequence_length, embed_size_per_head)`).
-            Contains pre-computed hidden-states (key and values in the attention blocks) that can be used (see
-            :obj:`past_key_values` input) to speed up sequential decoding.
-        hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_hidden_states=True`` is passed or when ``config.output_hidden_states=True``):
-            Tuple of :obj:`torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer)
+            If :obj:`past_key_values` is used only the last hidden-state of the
+            sequences of shape :obj:`(batch_size, 1, hidden_size)` is output.
+        past_key_values (:obj:`List[torch.FloatTensor]`, `optional`, returned when
+        ``use_cache=True`` is passed or when ``config.use_cache=True``):
+            List of :obj:`torch.FloatTensor` of length :obj:`config.n_layers`, with
+            each tensor of shape :obj:`(2, batch_size, num_heads, sequence_length,
+            embed_size_per_head)`).
+            Contains pre-computed hidden-states (key and values in the attention
+            blocks) that can be used (see:obj:`past_key_values` input) to speed up
+            sequential decoding.
+        hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when
+        ``output_hidden_states=True`` is passed or when
+        ``config.output_hidden_states=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for the output of the
+            embeddings + one for the output of each layer)
             of shape :obj:`(batch_size, sequence_length, hidden_size)`.
-            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
-        attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_attentions=True`` is passed or when ``config.output_attentions=True``):
-            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape :obj:`(batch_size, num_heads,
-            sequence_length, sequence_length)`.
-            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
-            heads.
-        cross_attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_attentions=True`` and ``config.add_cross_attention=True`` is passed or when ``config.output_attentions=True``):
-            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape :obj:`(batch_size, num_heads,
-            sequence_length, sequence_length)`.
-            Attentions weights of the decoder's cross-attention layer, after the attention softmax, used to compute the
-            weighted average in the cross-attention heads.
+            Hidden-states of the model at the output of each layer plus the initial
+            embedding outputs.
+        attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when
+        ``output_attentions=True`` is passed or when
+        ``config.output_attentions=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+            :obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
+            Attentions weights after the attention softmax, used to compute the
+            weighted average in the self-attention heads.
+        cross_attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned
+        when ``output_attentions=True`` and ``config.add_cross_attention=True``
+         is passed or when ``config.output_attentions=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+            :obj:`(batch_size, num_heads,sequence_length, sequence_length)`.
+            Attentions weights of the decoder's cross-attention layer, after the
+            attention softmax, used to compute the weighted average in the
+            cross-attention heads.
         pooled_hidden_state
             Pooled hidden states.
     """

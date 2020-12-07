ff9a34c73f4ba1fb944efd2a15554dd658f94230
rabeeh <rabeeh@google.com>
2020-12-07 14:25:15 +0100

    updated submit

--- seq2seq/temp/submit_jobs.py
+++ seq2seq/temp/submit_jobs.py
@@ -16,19 +16,26 @@ def submit_job(curr_job, filename, outpath_base):
        f.write(curr_job)
     os.system("qsub -V -N {0} -e {1}.err -o {1}.out template.job".format(filename, os.path.join(outpath_base, filename)))
 
-outpath_base = "/idiap/temp/rkarimi/internship"
+outpath_base = "/idiap/temp/rkarimi/internship/sampled/"
 if not os.path.exists(outpath_base):
     os.makedirs(outpath_base)
 def run_experiment():
-    config_path="/idiap/user/rkarimi/dev/ruse/seq2seq/configs/meta_adapter_experiments/gpu"
-    configs = ["finetune-2e-5.json",
-               "finetune-3e-4.json",
-               "paramteric-meta-adapter-1e-2.json",
-               "paramteric-meta-adapter-3e-2.json",
-               "finetune-3e-3.json",
-               "finetune-3e-5.json",
-               "paramteric-meta-adapter-3e-1.json",
-               "paramteric-meta-adapter-3e-3.json"]
+    config_path="/idiap/user/rkarimi/dev/ruse/seq2seq/configs/meta_adapter_experiments/train_num_samples_gpu"
+
+
+    configs = [
+       "finetune-100.json",
+       "finetune-500.json ",
+       "finetune-1000.json",
+       "finetune-1500.json",
+       "finetune-2000.json",
+       "finetune-2400.json",
+       "paramteric-meta-adapter-100.json",
+       "paramteric-meta-adapter-500.json",
+       "paramteric-meta-adapter-1000.json",
+       "paramteric-meta-adapter-1500.json ",
+       "paramteric-meta-adapter-2000.json"
+       "paramteric-meta-adapter-2400.json"]
     for config in configs:
       job_string=job_string_base+'''python finetune_t5_trainer.py  {0}'''
       curr_job = job_string.format(os.path.join(config_path, config))

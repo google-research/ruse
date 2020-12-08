72d37d09f476ab55be093674bd9d89fb170963c3
rabeeh <rabeeh@google.com>
2020-12-08 23:17:21 +0100

    updated launch

--- seq2seq/launch.py
+++ seq2seq/launch.py
@@ -5,7 +5,7 @@ def run_jobs(configs_dir, config_name, job_name):
     command="/google/bin/releases/cloud-alphabetcloud-xcloud/xcloud_cli/xcloud_cli.par google/launch_xla.py  -- --config_path {0} --job_name {1} --num_gpus 1".format(config_path, job_name)
     os.system(command)
 
-
+"""
 # submit adapters
 configs_dir = "configs/mixtures1-task/gpu/adapter/"
 configs=[
@@ -18,7 +18,6 @@ configs=[
 for config in configs:
     run_jobs(configs_dir, config, "mixture1-"+config[:-5])
 
-"""
 # submit finetune.
 configs_dir = "configs/mixtures1/gpu/finetune"
 configs=["finetune-2e-5.json",
@@ -27,7 +26,6 @@ configs=["finetune-2e-5.json",
          "finetune-3e-5.json"]
 for config in configs:
     run_jobs(configs_dir, config, "mixture1-"+config[:-5])
-"""
 
 # submit adapters
 configs_dir = "configs/mixtures2-task/gpu/adapter/"
@@ -39,8 +37,6 @@ configs=[
          "paramteric-meta-adapter-3e-4.json"]
 for config in configs:
     run_jobs(configs_dir, config, "mixture2-"+config[:-5])
-
-"""
 # submit finetune.
 configs_dir = "configs/mixtures2/gpu/finetune"
 configs=["finetune-2e-5.json",
@@ -50,3 +46,27 @@ configs=["finetune-2e-5.json",
 for config in configs:
     run_jobs(configs_dir, config, "mixture2-"+config[:-5])
 """
+
+
+# submit adapters
+configs_dir = "configs/mixtures1-task/gpu/adapter_no_task_emb/"
+configs=[
+         "paramteric-meta-adapter-1e-2.json",
+         "paramteric-meta-adapter-3e-1.json",
+         "paramteric-meta-adapter-3e-2.json",
+         "paramteric-meta-adapter-3e-3.json",
+         "paramteric-meta-adapter-3e-4.json"
+         ]
+for config in configs:
+    run_jobs(configs_dir, config, "mixture1-noinit-"+config[:-5])
+
+# submit adapters
+configs_dir = "configs/mixtures2-task/gpu/adapter_no_task_emb/"
+configs=[
+         "paramteric-meta-adapter-1e-2.json",
+         "paramteric-meta-adapter-3e-1.json",
+         "paramteric-meta-adapter-3e-2.json",
+         "paramteric-meta-adapter-3e-3.json",
+         "paramteric-meta-adapter-3e-4.json"]
+for config in configs:
+    run_jobs(configs_dir, config, "mixture2-noinit-"+config[:-5])

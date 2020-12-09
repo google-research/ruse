efd8e8f11e16deefdeaa6b281435ffccc3da9408
rabeeh <rabeeh@google.com>
2020-12-10 00:36:21 +0100

    added configs for no layernorm

--- seq2seq/clean_launch.py
+++ seq2seq/clean_launch.py
@@ -140,15 +140,26 @@ job_prefix = "mix1-t5-scratch"
 short_keys = ["lr"]
 sweep = collections.OrderedDict({'learning_rate': [1e-2, 3e-1, 3e-2, 3e-3, 3e-4]})
 do_sweep(basic_config_path, sweep, short_keys, job_prefix)
-"""
+
 
 basic_config_path = "configs/experiments/mixture2/t5-scratch.json"
 job_prefix = "mix2-t5-scratch"
 short_keys = ["lr"]
 sweep = collections.OrderedDict({'learning_rate': [1e-2, 3e-1, 3e-2, 3e-3, 3e-4]})
 do_sweep(basic_config_path, sweep, short_keys, job_prefix)
+"""
+
+
+basic_config_path="configs/experiments/mixture1/meta-task-emb-no-layer-norm.json"
+job_prefix = "mix1-meta-task-emb-no-ln"
+short_keys = ["lr"]
+sweep = collections.OrderedDict({'learning_rate': [1e-2, 3e-1, 3e-2, 3e-3, 3e-4]})
+do_sweep(basic_config_path, sweep, short_keys, job_prefix)
 
 
 
-
-
+basic_config_path="configs/experiments/mixture2/meta-task-emb-no-layer-norm.json"
+job_prefix = "mix2-meta-task-emb-no-ln"
+short_keys = ["lr"]
+sweep = collections.OrderedDict({'learning_rate': [1e-2, 3e-1, 3e-2, 3e-3, 3e-4]})
+do_sweep(basic_config_path, sweep, short_keys, job_prefix)

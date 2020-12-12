429ee5c99ed6fad6e683b8862c8c00f723791fa1
rabeeh <rabeeh@google.com>
2020-12-12 13:12:17 +0100

    removed gcl launchs to temp

--- launch.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system("/google/bin/releases/cloud-alphabetcloud-xcloud/xcloud_cli/xcloud_cli.par seq2seq/google/launch_xla.py  -- --config_path configs/meta_adapter_experiments/train_num_samples_gpu/finetune-100.json  --job_name test1 --num_gpus 1")
-
-

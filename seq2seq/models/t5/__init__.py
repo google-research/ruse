033827854cb4b2b2552b57df624c41b96817f9a3
rabeeh <rabeeh@google.com>
2020-12-13 23:30:18 +0100

    restructured model dir

--- seq2seq/models/t5/__init__.py
+++
@@ -1,4 +0,0 @@
-from .modeling_t5 import T5ForConditionalGeneration
-from .projections import AutoProjection, PROJECTION_MAPPING
-from .poolings import AutoPooling, POOLING_MAPPING
-from .configuration_t5 import T5Config

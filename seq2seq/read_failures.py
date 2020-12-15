5300c40f774088e51c2d0ec6370d309200fe22e7
rabeeh <rabeeh@google.com>
2020-12-15 15:27:33 +0000

    add a script to get list of failures

--- seq2seq/read_failures.py
+++ seq2seq/read_failures.py
@@ -1,4 +1,14 @@
-import pandas as pd
-df = pd.read_csv ('text', sep=' ')
-print(df.head())
-print(df['JobName'])
+with open("text") as f:
+    lines = f.readlines()
+
+job_name_ind = 0
+status_ind = 20
+failed_jobs = []
+for line in lines:
+    splits = line.split()
+    job_id = splits[0].rstrip()[9:]
+    status = splits[-1].rstrip()
+    date = splits[2].rstrip()
+    if status == "FAILED" and date == "2020-12-15":
+        failed_jobs.append(job_id)
+print(failed_jobs)

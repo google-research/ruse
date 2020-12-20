a29e49606ea87597cc1428616c61f396ee4d339e
rabeeh <rabeeh@google.com>
2020-12-20 20:34:23 +0000

    updated command

--- seq2seq/train_distribued.sh
+++ seq2seq/train_distribued.sh
@@ -15,49 +15,48 @@
 
 
 export BS=4; CUDA_VISIBLE_DEVICES=0,1  USE_TF=0   python -m torch.distributed.launch --nproc_per_node=2 --master_port=9910  finetune_t5_trainer.py \
-	--model_name_or_path t5-small\
-	--tokenizer_name t5-small\
-	--learning_rate  1e-2\
-	--output_dir outputs/test
-	--max_source_length 128\
-	--max_target_length": 128 \
-	--val_max_target_length":128
-		--test_max_target_length":128,
-		--num_train_epochs": 10,
-"warmup_steps": 500,
-"eval_steps": 200,
-"overwrite_output_dir": true,
-"tasks": ["scitail", "boolq"],
-"eval_tasks": ["rte", "boolq"],
-"sampling": true,
-"label_smoothing": 0.1,
-"freeze_encoder":false,
-"freeze_embeds":false,
-"per_device_train_batch_size":64,
-"per_device_eval_batch_size":64,
-"save_steps": 20,
-"logging_first_step":true,
-"logging_steps": 200,
-"save_total_limit": 1,
-"train_adapters": true,
-"adapter_config_name":"parametric-meta-adapter",
-"temperature": 10,
-"do_eval": true,
-"predict_with_generate": true,
-"n_train": 10,
-"task_embedding_dir":"test_data/task_embeddings/n-train-all",
-"task_embedding_dim": 512,
-"n_val": 10,
-"n_train": 10,
-"do_finetune": true,
-"do_train": true,
-"n_finetune": 100,
-"eval_output_dir": "outputs/eval_test",
-"reduction_factor": 16,
-"non_linearity": "relu",
-"train_task_embeddings": true,
-"projected_task_embedding_dim": 512,
-"add_adapters_in_decoder": false,
-"unfreeze_lm_head": true,
-"unfreeze_layer_norms": true
-
+	--model_name_or_path t5-small \
+	--tokenizer_name t5-small \
+	--learning_rate  1e-2 \
+	--output_dir outputs/test \
+	--max_source_length 128 \
+	--max_target_length 128 \
+	--val_max_target_length 128 \
+		--test_max_target_length 128 \
+		--num_train_epochs 10 \
+		--warmup_steps 500 \
+		--eval_steps 200 \
+		--overwrite_output_dir true \
+		--tasks  ["scitail", "boolq"] \
+		--eval_tasks ["rte", "boolq"] \
+		--sampling true \
+		--label_smoothing 0.1 \
+		--freeze_encoder false \
+		--freeze_embeds false \
+		--per_device_train_batch_size" 64 \
+		--per_device_eval_batch_size" 64 \
+		--save_steps": 20 \
+		--logging_first_step true \
+		--logging_steps 200 \
+		--save_total_limit 1 \
+		--train_adapters true \
+		--adapter_config_name parametric-meta-adapter \
+		--temperature 10 \
+		--do_eval \
+		--predict_with_generate true \
+		--n_train  10 \
+		--task_embedding_dir test_data/task_embeddings/n-train-all \
+		--task_embedding_dim  512 \
+		--n_val 10 \
+		--n_train 10 \
+		--do_finetune true \
+		--do_train true \
+		--n_finetune 100 \
+		--eval_output_dir outputs/eval_test \
+		--reduction_factor 16 \
+		--non_linearity relu \
+		--train_task_embeddings true \
+		--projected_task_embedding_dim  512
+		--add_adapters_in_decoder false \
+		--unfreeze_lm_head \
+		--unfreeze_layer_norms

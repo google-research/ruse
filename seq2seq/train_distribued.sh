7f8b0266cb9b5bb11220cff6a3ba8e448216c574
rabeeh <rabeeh@google.com>
2020-12-22 17:54:18 +0000

    updated

--- seq2seq/train_distribued.sh
+++ seq2seq/train_distribued.sh
@@ -1,16 +1,17 @@
 #export BS=4; CUDA_VISIBLE_DEVICES=0,1,2,3  USE_TF=0   
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-02-r-8-l-true
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-02-r-8-l-false
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-03-r-8-l-true
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-05-r-8-l-false.json
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-05-r-8-l-false.json
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-05-r-8-l-false.json
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-02-r-8-l-true.json
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-02-r-8-l-false.json
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-03-r-8-l-true.json
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-03-r-8-l-false.json
 
-### name  xla-lr-3e-03-r-8-l-false
-### name  xla-lr-3e-04-r-8-l-true
-### name  xla-lr-3e-04-r-8-l-false
-### name  xla-lr-2e-05-r-8-l-true
-### name  xla-lr-2e-05-r-8-l-false
-### name  xla-lr-3e-05-r-8-l-true
-### name  xla-lr-3e-05-r-8-l-false
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-04-r-8-l-false.json
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-2e-05-r-8-l-true.json
+
+
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-04-r-8-l-true.json
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-2e-05-r-8-l-false.json
+
+
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-05-r-8-l-true.json
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=9918  finetune_t5_trainer.py temp_configs/xla-lr-3e-05-r-8-l-false.json
 

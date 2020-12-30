f2cbd4b88e74c28a45651ccf6479142a7bf5eaa5
rabeehkarimimahabadi <73364383+rabeehkarimimahabadi@users.noreply.github.com>
2020-12-30 19:10:35 +0000

    Hf codes (#19)
    * implements task, task-collator, task-sampler
    
    * updated
    
    * updated task cl
    
    * removed task-collator
    
    * updated hf codes
    
    * removed data

--- seq2seq/third_party/utils/__init__.py
+++ seq2seq/third_party/utils/__init__.py
@@ -4,13 +4,14 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    https://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from .utils import (
   calculate_rouge,
   calculate_bleu,
@@ -21,4 +22,5 @@ from .utils import (
   lmap,
   save_json,
   write_txt_file,
-  label_smoothed_nll_loss)
+  label_smoothed_nll_loss,
+  TaskCollator)
--- seq2seq/third_party/utils/utils.py
+++ seq2seq/third_party/utils/utils.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The HuggingFace Team. All rights reserved.
+# Copyright 2020 The HuggingFace Team. All rights reserved
 # Copyright 2020 Google LLC
 # Modified from the original HuggingFace version.
 #
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import itertools
 import json
 import linecache
@@ -658,3 +659,57 @@ def check_output_dir(args, expected_items=0):
             f"has {len(os.listdir(args.output_dir))} items in it (expected {expected_items} items). "
             "Use --overwrite_output_dir to overcome."
         )
+
+
+
+
+class TaskCollator:
+  """Implements task-collator to collate the samples in each batch."""
+  def __init__(self, tokenizer, data_args, tpu_num_cores=None):
+    self.tokenizer = tokenizer
+    self.pad_token_id = tokenizer.pad_token_id
+    assert (
+        self.pad_token_id is not None
+    ), f"pad_token_id is not defined for ({self.tokenizer.__class__.__name__}), it must be defined."
+    self.data_args = data_args
+    self.tpu_num_cores = tpu_num_cores
+
+  def __call__(self, batch) -> Dict[str, torch.Tensor]:
+    # because of padding="longest" this does not work to be done in dataset part.
+    batch = self._encode(batch)
+    input_ids, attention_mask, labels = (
+      batch["input_ids"],
+      batch["attention_mask"],
+      batch["labels"],
+    )
+    decoder_input_ids = self._shift_right_t5(labels)
+    output_batch = {
+      "input_ids": input_ids,
+      "attention_mask": attention_mask,
+      "decoder_input_ids": decoder_input_ids,
+      "labels": labels,
+    }
+    output_batch["task"] = batch["task"]
+    return output_batch
+
+  def _shift_right_t5(self, input_ids):
+    # shift inputs to the right
+    shifted_input_ids = input_ids.new_zeros(input_ids.shape)
+    shifted_input_ids[..., 1:] = input_ids[..., :-1].clone()
+    shifted_input_ids[..., 0] = self.pad_token_id
+    return shifted_input_ids
+
+  def _encode(self, batch) -> Dict[str, torch.Tensor]:
+    batch_encoding = self.tokenizer.prepare_seq2seq_batch(
+      [x["src_texts"] for x in batch],
+      tgt_texts=[x["tgt_texts"] for x in batch],
+      max_length=self.data_args.max_source_length,
+      max_target_length=self.data_args.max_target_length,
+      padding="max_length" if self.tpu_num_cores is not None else "longest",  # TPU hack
+      return_tensors="pt"
+    )
+    tasks = [x["task"] for x in batch]
+    # There should be only one task per batch.
+    assert (len(set(tasks)) == 1)
+    batch_encoding["task"] = tasks[0]
+    return batch_encoding.data
